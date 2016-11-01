3d42302ad1b69c4a7e76b673f420676d72936472
MarchCastro <march.castrof@gmail.com>
2016-10-31 21:11:10 -0600

    Reseteando repositorio

--- WebPractica2/phps/configBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servidorBD = "127.0.0.1";
-	$usrBD = "root";
-	$passBD = "postcardfrom1952";
-	$nombreBD = "sem20171";
-	
-	$conexion = mysqli_connect($servidorBD, $usrBD, $passBD, $nombreBD);
-	mysqli_query($conexion,"SET NAMES 'utf8'");
-?>

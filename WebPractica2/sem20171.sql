dd4bf16e9492f79a3d897577bfe4255a83a482e6
MarchCastro <march.castrof@gmail.com>
2016-11-01 23:35:51 -0600

    agregando BD con tabla

--- WebPractica2/sem20171.sql
+++ WebPractica2/sem20171.sql
@@ -1,25 +1,53 @@
-/*
-SQLyog Community v12.2.4 (32 bit)
-MySQL - 5.7.11 : Database - sem20171
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 5.7.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sem20171
+-- ------------------------------------------------------
+-- Server version	5.7.13-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`sem20171` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `sem20171`;
+--
+-- Table structure for table `administrador`
+--
 
-/*Table structure for table `estudiante` */
+DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrador` (
+  `idAdmin` int(11) NOT NULL AUTO_INCREMENT,
+  `noEmpleado` varchar(11) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`idAdmin`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrador`
+--
+
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (1,'123','4d186321c1a7f0f354b297e8914ab2400','Juan Peredo');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiante`
+--
 
 DROP TABLE IF EXISTS `estudiante`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `estudiante` (
   `boleta` varchar(10) NOT NULL,
   `nombre` varchar(64) NOT NULL,
@@ -28,15 +56,25 @@ CREATE TABLE `estudiante` (
   `curp` varchar(18) NOT NULL,
   PRIMARY KEY (`boleta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `estudiante` */
+--
+-- Dumping data for table `estudiante`
+--
 
-insert  into `estudiante`(`boleta`,`nombre`,`apellidos`,`correo`,`curp`) values 
-('2016630001','Juan Luis','Ortiz Pérez','juan@juan.com','ABCD123456HDFRMN00'),
-('2016630002','Miguel Ángel','Méndez Méndez','miguel@miguel.com','ABCD123456HDFRMN01'),
-('97630005','José Antonio','Ortiz Ramírez','jaor@jaor.com','ABCD123456HDFRMN03');
+LOCK TABLES `estudiante` WRITE;
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+INSERT INTO `estudiante` VALUES ('2016630001','Juan Luis','Ortiz Pérez','juan@juan.com','ABCD123456HDFRMN00'),('2016630002','Miguel Ángel','Méndez Méndez','miguel@miguel.com','ABCD123456HDFRMN01'),('97630005','José Antonio','Ortiz Ramírez','jaor@jaor.com','ABCD123456HDFRMN03');
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-01 23:35:29
